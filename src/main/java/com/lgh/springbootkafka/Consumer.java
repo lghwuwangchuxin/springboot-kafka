4fd3733e00f25ba06c007b6cd785687cc838af94
lgh <lghwuwangchuxin@163.com>
2019-01-29 11:11:29 +0800

    Initial commit

---
+++ src/main/java/com/lgh/springbootkafka/Consumer.java
@@ -0,0 +1,42 @@
+package com.lgh.springbootkafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author ：lgh
+ * @date ：Created in 2019/1/29 10:29
+ * @description：消费者
+ * @modified By：
+ */
+@Component
+public class Consumer {
+    /**
+     * 有消息就读取,只读取消息value
+     */
+    @KafkaListener(topics = {"test13"})
+    public void receiveMessage(String message){
+        //收到通道的消息之后执行秒杀操作
+        System.out.println(message);
+    }
+
+    /**
+     * 有消息就读取,批量读取消息value
+     */
+    @KafkaListener(topics = "test12")
+    public void onMessage(List<String> crs) {
+        for(String str : crs){
+            System.out.println("test12:" + str);
+        }
+    }
+    /**
+     * 有消息就读取,读取消息topic，offset，key，value等信息
+     */
+    @KafkaListener(topics = "test14")
+    public void listenT1(ConsumerRecord<?, ?> cr){
+        System.out.println("listenT1收到消息,topic:>>>" + cr.topic() + "  offset:>>" + cr.offset()+ "  key:>>" + cr.key() + "  value:>>" + cr.value());
+    }
+}
---
+++ src/test/java/com/lgh/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.lgh.springbootkafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootKafkaApplicationTests {
+    /**
+     * 生产者测试
+     */
+    @Test
+    public void TestProducer() {
+
+    }
+
+}
+
